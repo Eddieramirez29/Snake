e5db739db5a3d6f226045338ee64581d51076588
Eduardo <tkd13000@gmail.com>
2024-07-22 19:18:24 -0600

    stop function and restart position

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const snake = document.getElementById('snake');
 //Initial position for the "snake"
-let posX = 376; 
+let posX = 376;
 let posY = 159;
+
+let intervalId = 0
 
 
 //This funcion simulates movement of the sanek
@@ -13,14 +15,29 @@ function movement()
     posX = posX + 1;
 }
 
+//This funcion restarts to initial position
+function restartPosition()
+{
+    posX = 376;
+    posY = 159;
+    snake.style.left = posX + 'px';
+    snake.style.top = posY + 'px';
+}
+
 document.addEventListener('keydown', function(event) 
 {
     //When clicking "Enter" key, the game will start
     if (event.key === 'Enter') 
     { 
         //delay 500 ms
-       setInterval(movement, 5);
+        intervalId = setInterval(movement, 5);
     }
+    //Restart to initial position
+    else if (event.key === 'r' || event.key === 'R') 
+            { 
+                clearInterval(intervalId);//Stop snake
+                restartPosition();
+            }
 });
 
 
