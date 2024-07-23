6ad555ac6c602ca1cd571b1c0d8e176247996b82
Eduardo <tkd13000@gmail.com>
2024-07-22 19:38:45 -0600

    direction are defined

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ const snake = document.getElementById('snake');
 let posX = 376;
 let posY = 159;
 
+//Initial direction
+let direction = 'right';
+
 let intervalId = 0
 //This funcion simulates movement of the sanek
 function movement()
 {
+    switch(direction) 
+    {
+        case 'right':
+            posX += 1;
+            posY += 0;
+            break;
+        case 'left':
+            posX -= 1;
+            break;
+        case 'up':
+            posY -= 1;
+            posX += 0;
+            break;
+        case 'down':
+            posY += 1;
+            posX += 0;
+            break;
+    }
     snake.style.left = posX + 'px';
     snake.style.top = posY + 'px';
 
-    posX = posX + 1;
 }
 
 //This funcion restarts to initial position
@@ -31,7 +51,7 @@ document.addEventListener('keydown', function(event)
         //delay 5 ms
         // Prevent multiple intervals
         if (intervalId === 0) {
-            intervalId = setInterval(movement, 5);
+            intervalId = setInterval(movement, 50);
         }
     }
     //Restart to initial position
@@ -40,6 +60,22 @@ document.addEventListener('keydown', function(event)
                 clearInterval(intervalId);//Stop snake
                 intervalId = 0
                 restartPosition();
+            }
+    else if (event.key === 'ArrowRight') 
+            {
+                direction = 'right';
+            } 
+    else if (event.key === 'ArrowLeft') 
+            {
+                direction = 'left';
+            } 
+    else if (event.key === 'ArrowUp') 
+            {
+                direction = 'up';
+            } 
+    else if (event.key === 'ArrowDown') 
+            {
+                direction = 'down';
             }
 });
 
